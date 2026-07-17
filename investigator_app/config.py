f023581a85ed2d5313164833ef651764b5813ecc
rinda0211 <85477658+rinda0211@users.noreply.github.com>
2026-07-17 09:25:38 +0900

    Update config.py

--- investigator_app/config.py
+++ investigator_app/config.py
@@ -1,3 +1,3 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///investigator_list.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_LHsqmW5RaFd0@ep-twilight-lake-azyuo1ph-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
