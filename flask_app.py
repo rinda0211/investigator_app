79d4369aaac87966a05d27458308ccc6ae65610f
rinda0211 <85477658+rinda0211@users.noreply.github.com>
2026-07-17 09:15:11 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from investigator_app import app
 
 app = Flask(__name__)
-app.config.from_object('investigator_app.config')
 
-db = SQLAlchemy(app)
-from .models import investigator
-
-import investigator_app.views
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def hello():
+    return "こんにちは！これはRenderで公開されたアプリです。"
